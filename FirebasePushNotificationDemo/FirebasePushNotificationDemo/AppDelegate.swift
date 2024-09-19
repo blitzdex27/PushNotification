d28f51abf88bdf3f2839b99b91eb37bb9d412c42
dexter <blitzdex27@gmail.com>
2024-09-19 16:40:40 +0800

    compelte firebase cloud messaging

--- FirebasePushNotificationDemo/FirebasePushNotificationDemo/AppDelegate.swift
+++ FirebasePushNotificationDemo/FirebasePushNotificationDemo/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        setupRemoteNotification()
         return true
     }
 
@@ -32,4 +37,65 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      if let fcmToken {
+          /// `fcmToken` is used for sending notification to this device.
+          print("xx-->> fcmToken \(fcmToken)")
+      }
+  }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        // handle notification as soon as it was received
+        completionHandler(.banner)
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        let notification = response.notification
+        switch UIApplication.shared.applicationState {
+        case .active: // taps from foreground
+            // Handle notification
+            break
+        case .inactive: // taps from inactive state
+            // Handle notification
+            break
+        case .background: // taps from terminated state
+            // Handle notification
+            break
+        default:
+            // Handle notification or use fatalError()
+            break
+        }
+        completionHandler()
+    }
+    
+    private func setupRemoteNotification() {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { authenticated, error in
+            if let error {
+                print("xx-->> Error: \(error.localizedDescription)")
+            }
+            
+            guard authenticated else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+
 }
